7f6a7005b2888595f5960dbd70b362ca149d9ae4
Jimena Belen <jiment4@hotmail.com>
2023-10-12 17:09:46 -0300

    data

---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+import Image
+
+
+
+const movimientos = [
+    {
+      img: Image1,
+      title: 'Proto-Renacimiento',
+      years: "1300–1400",
+      paragraph: "Primer Renacimiento, o Renacimiento primitivo, o Renacimiento temprano o Renacimiento inicial o Prerrenacimiento… En torno al siglo XIV algo cambia en Europa. Se supone que estamos en la edad media, pero algo empieza a revolucionarse en la política (se consolidan los estados europeos), la sociedad (la burguesía empieza a ascender), la economía (es aquí cuando nace el capitalismo)… y por supuesto la cultura. De pronto es el hombre, y no Dios, el centro de todo. De pronto artistas y artesanos son valorados. De pronto se empieza a usar el naturalismo, la armonía y las proporciones matemáticas para crear belleza. El artista se vuelve un científico, debe saber de muchas disciplinas para desarrollar su oficio. Ya no practica solo actividades viles y mecánicas. Ahora su oficio empieza a elevarse. El artista empieza a soñar. En el proto-renacimiento estamos entre dos mundos. Estamos en el gótico (recordemos que este es un nombre a posteriori, pues para los renacentistas este era un arte de bárbaros, de godos), pero se empieza a sentir el Quattrocento.",
+      obras: [
+        {
+            title: "Extracción de la piedra de la locura",
+            author: "Hyeronimus Bosch",
+            location: "Países Bajos, 1480"
+        },
+        {
+            title: "La matanza de los inocentes",
+            author: "Giotto",
+            location: "Italia, 1306"
+        },
+        {
+            title: "El descendimiento",
+            author: "Rogier Van der Weyden",
+            location: "Flandes, 1435"
+        },
+        {
+            title: "El banquete de Herodes",
+            author: "Giotto",
+            location: "Italia, 1315"
+        },
+        {
+            title: "Lamentación sobre Cristo muerto",
+            author: "Giotto",
+            location: "Italia, 1306"
+        },
+        {
+            title: "El matrimonio Arnolfini",
+            author: "Jan Van Eyck",
+            location: "Países Bajos, 1434"
+        }
+      ],
+    }
+]
+
+export default movimientos;
